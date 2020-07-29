c46195a3724fa2b8b534a94dc54198c138c390ae
ty-vin <tyvin888@gmail.com>
2020-07-28 20:58:59 -0400

    crazy object complete

--- git_homework/w3d3_hmwk/answers.js
+++ git_homework/w3d3_hmwk/answers.js
@@ -64,7 +64,7 @@ const rainbowColors = ['red', 'orange', 'yellow', 'green', 'blue', 'indego', 'vi
 
 // 2) Write code that will access "blue" from the rainbow array.
 
-console.log(rainbowColors[4]);
+console.log(rainbowColors[4]);  // logs blue (the 4th element) from the array 
 
 
 
@@ -83,11 +83,85 @@ const tV = {
 
 // 4) Write code that will access your hobby from the object that you just created.
 
-console.log(tV.hobby);
+console.log(tV.hobby);  // call the hobby element from the object
 
 // 5) Commit message: "took it easy"
 
 
+
+
+
+/////   Crazy Object!  /////
+
+
+
+const crazyObject = {
+  taco: [{meat: 'steak',
+         cheese: ['panela', 'queso', 'chihuahua']},
+         {meat: 'chicken',
+          salsa: ["pico", "hot", "hotter", "really hot", "really really hot", "omg my mouth is burning"]},
+        ],
+  larry: {
+    nicknames: ["LD", "Chicken Teriyaki Boyyyyyy"],
+    quotes: ["Pretty pretty prettayyyyy good", "Is that a parkinson's thing?", "women love a self confident bald man", "I'm a walking talking enigma"],
+    characters: [{
+      name: "Jeff",
+      occupation: "manager"
+    },
+    {
+      name: "funkhauser",
+      occupation: "tv dude"
+    },
+    {
+      name: "susie",
+      occupation: "jeffs wife",
+      favourtieHobby: "Swearing at Larry and Jeff"
+    },
+    ]
+  }
+}
+
+
+// Use crazy Object to log the following.
+
+
+// 1)  "omg my mouth is burning"
+
+console.log(crazyObject.taco[1].salsa[5]); // call the taco object and the salsa object from the array
+
+// 2) "Pretty pretty prettayyyyy good"
+
+console.log(crazyObject.larry.quotes[0]);  // logs larry array, quotes object, element '0' in quotes object
+
+// 3) "Swearing at Larry and Jeff"
+
+console.log(crazyObject.larry.characters[2].favourtieHobby); // logs larry array, characters element [2], favorite hobby for that character/element
+
+// 4) "Chicken Teriyaki Boyyyyyy"
+
+console.log(crazyObject.larry.nicknames[1]);  // logs crazyObject array element larry, nickname object '1'
+
+// 5) The object the contains the name funkhauser
+
+console.log(crazyObject.larry.characters[1].name);  // logs crazyObject array element larry, characters element '1', name of the element
+
+// 6) Add the quote "I'm trying to elevate small talk to medium talk" to Larry's quote array. Confirm that it was added.
+
+crazyObject.larry.quotes.push (" I'm trying to elevate small talk to medium talk "); // pushes the quote into the last place in the larry-quotes array
+console.log(crazyObject.larry.quotes);  //logs the push
+
+/*
+confirmation:
+
+  'Pretty pretty prettayyyyy good',
+  "Is that a parkinson's thing?",
+  'women love a self confident bald man',
+  "I'm a walking talking enigma",
+  " I'm trying to elevate small talk to medium talk "
+]
+*/
+
+// Commit Message: "crazy object complete"
 
 
 
