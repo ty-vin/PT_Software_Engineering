73d515950425a62a11197e355a9b4cdf3a151a66
ty-vin <tyvin888@gmail.com>
2020-08-08 21:14:45 -0400

    Combine objects, arrays, and functions

---
+++ git_homework/w4d2_hmwk/combining_datatypes.js
@@ -0,0 +1,153 @@
+
+
+
+//////////////    ///////////////    //////////////    ///////////////    //////////////    ///////////////
+
+// ~~~~  Section 2 Combining Datatypes  Combine objects, arrays, and functions  ~~~~
+
+
+// touch combining_datatypes.js and do your work for this section in this new file
+
+// 1) Create an object, called crayonBox, that has a property that is an array. 
+// The array should have the names of some crayons. Log one of the elements of that array.
+
+
+const crayonBox = {  // crayonBox object that holds the crayon array
+    crayons: ['brown', 'black', 'tan', 'yellow', 'cream'] // crayon array with natural crayon colors chosen
+}
+console.log(crayonBox.crayons[0]); // logs the 0 position element of the crayons array, within the crayonBox object. 
+                                    // that 0 position element is 'brown' [0,1,2,3,4,5,...]
+
+
+// 2) Create an object 'bottle' that has a property that is an object: 'cap'. 
+// cap can have properties like 'material': 'metal' or 'plastic', 'color': blue or white etc. 
+// Log one of the properties of that inner object.
+
+const bottle =  {            // object 'bottle' with the property 'cap'
+    cap: {                   // 'cap' properties 
+        material1: 'metal',     
+        color1: 'silver' 
+        }   
+    }        
+
+console.log(bottle.cap.color1); // logs the propery 'color1' inside the object 'cap' which is inside the object 'bottle'
+
+
+//  3) Create an array called 'receipt' that has at least one object in it. The inner objects should be items that 
+//  have a name and a price. Log one of the properties of that inner object.
+
+
+const receipt = [                           // receipt array with three inner objects " WallyWorld", "Maejor Store" & "Speedywayz"
+    {name: "WallyWorld", total: 75.21}
+    {name: "Maejor Store",total: 107.55} 
+    {name: "Speedywayz", total: 25.00}
+   ]
+ 
+ console.log(receipt[0].name);              //logs the name of the 0 object of array, "WallyWorld"
+
+
+
+//  4) Create an array called 'apartmentBuilding' that has an array as one of its elements, 
+//  the inner array should be the names of the 'tenants'. Log one of the elements of the inner array.
+
+
+const apartmentBuilding = [         // apartmentBuilding array with two arrays as elements inside of it
+    buildingWing = ['North', 'South', 'East', 'West'],              // inner array0 buildingWing, with 4 elements
+    tenants = ['Hickory Smoke', 'Jaxson Five', 'Aaround Tuit', 'Justin Case'],  // inner array1 tenants,with 4 elements
+]
+console.log(apartmentBuilding[1][2]);      // logs the [1] inner array (tenants), and [2] element of inner array1 ('Aaround Tuit')
+                                            // w/n the apartmentBuilding array 
+
+
+
+
+// Commit: "Combine objects, arrays, and functions"
+
+
+
+
+
+
+
+
+//////////////    ///////////////    //////////////    ///////////////    //////////////    ///////////////
+
+ // ~~~~~   Combine objects, arrays, and functions more than one level deep  ~~~~~~
+
+// 1) Create a function knit that returns an object that has the following kinds of properties 
+// item: scarf, size : 6ft etc. Log a value of that object 
+// (hint: call the function and then call a property on the return value).
+
+
+
+// 2) Create a function crayonSelector that returns an object that has an array (you can reuse your crayonBox object). 
+// Log one of the elements of that array.
+
+
+
+
+// 3) Create a function powerButton that returns a function called options - 
+// options should console.log a simple message like select a song. 
+// Call that inner function
+
+
+
+
+// Commit: "Combine objects, arrays, and functions more than one level deep"
+
+
+
+//////////////    ///////////////    //////////////    ///////////////    //////////////    ///////////////
+
+/*
+ // ~~~~~~  Model a Vending Machine Model a vending machine  ~~~~~
+
+a vending machine is an object
+it has an array of snacks (make 3 snacks)
+snacks are objects that have a name and a price
+a vending machine has a function vend that allows user to enter the array position (a number) of the snack and then that snack will be returned
+Be able to call vendingMachine.vend() with a valid integer to return a snack
+
+*/
+
+
+
+
+/*
+
+
+
+//////////////    ///////////////    //////////////    ///////////////    //////////////    ///////////////
+
+// ~~~~~~~~~~      Callbacks        ~~~~~~~~~~ 
+
+Make a function add that takes two arguments (numbers) and sums them together
+Make a function subtract that takes two arguments (numbers) and subtracts them
+Make a function multiply that takes two arguments and multiplies them
+Make a function divide that takes two arguments and divides them
+Make a function calculate that takes three arguments. Assume the two arguments are a number ie num1, num2 and a function called operates (a callback).
+Make it so that when calculate is invoked, the callback "operates" on the numbers and returns the value.
+Call calculate 4 times, each time using one of the operation functions you wrote
+Function definition placement
+Clean up this code, so that it works and has function definitions in the correct place
+
+bar();
+const bar = () => {
+    console.log('bar here');
+}
+foo();
+
+const foo = () => {
+    console.log('foo here');
+}
+Error reading
+What is meant by the error(s) this produces?
+
+foo();
+
+const foo ()=>{
+    console.log('hi');
+}
+
+
+*/
