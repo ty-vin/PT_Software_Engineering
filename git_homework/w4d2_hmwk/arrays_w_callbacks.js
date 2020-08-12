4e41eb1bb93d40df12bad64eac14ed15fb16d609
ty-vin <tyvin888@gmail.com>
2020-08-12 01:16:26 -0400

    working on arrays & callbacks.js

--- git_homework/w4d2_hmwk/arrays_w_callbacks.js
+++ git_homework/w4d2_hmwk/arrays_w_callbacks.js
@@ -25,31 +25,61 @@ const panagram = ['The', 'quick', 'brown', 'fox', 'jumps', 'over', 'the', 'lazy'
 
 // The first question is for the numbers array. The second question is for the words array.
 
-
 // You don't have to write an answer to the thought questions.
 
 // ------ ----- ----- Every: ------ ----- ----- 
 // Determine if every number is greater than or equal to 0
-// determine if every word shorter than 8 characters
+
+const greaterThan = (num) => {              // function that when called, returns all numbers >= to 0 from nums array
+    return (num >= 0)    
+}                                           // from the nums array
+  // console.log(nums.every(greaterThan));   // returns results off called function as true
+
+
+// determine if every word is shorter than 8 characters
+const panagramWord = (word) => {              // shortWord function that when called, returns every short word < 8 characters
+    return (word.length < 8)                  // from the panagram array
+}
+ // console.log(panagram.every(panagramWord));   // logs results of every word in the array < 8 called using the function
+
 
 
 // ------ ----- -----  Filter:  ------ ----- ----- 
 // filter the array for numbers less than 4
+
+const underFour = (num) => {               // function that calls the numbers of the array that are < than 4: 0, 1, 2, 3 via filter
+    return (num < 4 )
+}     
+ // console.log(nums.filter(underFour));     // logs the filtered results of the number array
+
+
 // filter words that have an even length
+const anEvenLength = (word)  => {           // function to call the even words from the word array
+    return (word.length % 2 === 0)          // logs word even word lengths:'over' and 'lazy'
+}
+ // console.log(panagram.filter(anEvenLength));
+
+
 
 
 // ------ ----- -----  Find: ------ ----- ----- 
 // Find the first value divisible by 5
+
+
 // find the first word that is longer than 5 characters
 
 
 // ------ ----- -----  Find Index:  ------ ----- ----- 
 // find the index of the first number that is divisible by 3
+
+
 // find the index of the first word that is less than 2 characters long
 
 
 // ------ ----- -----   For Each:  ------ ----- ----- 
 // console.log each value of the nums array multiplied by 3
+
+
 // console.log each word with an exclamation point at the end of it
 
 
@@ -57,22 +87,30 @@ const panagram = ['The', 'quick', 'brown', 'fox', 'jumps', 'over', 'the', 'lazy'
 // Thought Questions:
 
 // What happened to the original array?
+
+
 // Can you store the values from a forEach method in a new array?
 
 
 // ------ ----- -----  Map:  ------ ----- ----- 
 // make a new array of each number multiplied by 100
+
+
 // make a new array of all the words in all uppercase
 
 
 
 // Thought Questions:
 // What happened to the original array?
+
+
 // Can you store the values from a map method in a new array?
 
 
 // ------ ----- -----   Some:  ------ ----- ----- 
 // Find out if some numbers are divisible by 7
+
+
 // Find out if some words have the letter a in them
 
 
