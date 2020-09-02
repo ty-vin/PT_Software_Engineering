c8aafed9142e935269f457a9eea46ee5da54b940
ty-vin <tyvin888@gmail.com>
2020-09-01 20:59:44 -0400

    W4D2 HMWK

--- git_homework/w4d2_hmwk/arrays_w_callbacks.js
+++ git_homework/w4d2_hmwk/arrays_w_callbacks.js
@@ -65,52 +65,76 @@ const anEvenLength = (word)  => {           // function to call the even words f
 // ------ ----- -----  Find: ------ ----- ----- 
 // Find the first value divisible by 5
 
+const divFive = num => num % 5 === 0;
+console.log(nums.find(divFive))                       // logs 5
 
 // find the first word that is longer than 5 characters
 
+const biggerThanFive = b => b.length > 5
+console.log(panagram.find(biggerThanFive))          // logs undefined for all 5 or less than 
 
 // ------ ----- -----  Find Index:  ------ ----- ----- 
 // find the index of the first number that is divisible by 3
 
+const divThree = num => num % 3 === 0
+console.log(nums.findIndex(divThree))             // logs 2 using findIndex
 
 // find the index of the first word that is less than 2 characters long
+const underTwo = word => word.length < 2
+console.log(panagram.findIndex(underTwo))        // logs  -1  for all 3 or greater
 
 
 // ------ ----- -----   For Each:  ------ ----- ----- 
 // console.log each value of the nums array multiplied by 3
 
+nums.forEach(num => console.log(num * 3))    //uses forEach to log (3, 6, 9, 12, 15, 18, 21, 24, 27, 30, 0)
 
-// console.log each word with an exclamation point at the end of it
+
+// console.log each word with an exclamation point at the end of it   
+panagram.forEach(word => console.log(word + '!'))   // uses forEach to log (The!, quick!, brown!, fox!, jumps!, over!, the!, lazy!, dog!)
 
 
 
 // Thought Questions:
 
-// What happened to the original array?
+// What happened to the original array?  Unchanged
 
 
-// Can you store the values from a forEach method in a new array?
+// Can you store the values from a forEach method in a new array? yes
 
 
 // ------ ----- -----  Map:  ------ ----- ----- 
 // make a new array of each number multiplied by 100
 
+let newArryNums = []                            // declaration
+newArryNums.push(nums.map(num => num * 100))
+console.log(newArryNums)                        // logs the new array of numbers multiplied by 1oo (100, 200, 300, 400, 500 - 1000, 0)
+
 
 // make a new array of all the words in all uppercase
+
+let upper = []
+upper.push(panagram.map(word => word.toUpperCase()))
+console.log(upper)                                      // changes letters to uppercase logs uppercase of panagram array
 
 
 
 // Thought Questions:
-// What happened to the original array?
+// What happened to the original array? nothing
+ // console.log (panagram)
 
-
-// Can you store the values from a map method in a new array?
+// Can you store the values from a map method in a new array? Yes
 
 
 // ------ ----- -----   Some:  ------ ----- ----- 
+
 // Find out if some numbers are divisible by 7
+
+const divSeven = num => num % 7 === 0      //  looks for something divisible by 7
+console.log(nums.some(divSeven))          // logs true b/c there are numbers / by 7
 
 
 // Find out if some words have the letter a in them
 
-
+const lettA = word => word.includes('a')        // look for words that 'a'
+console.log(panagram.some(lettA))               // logs true: some words have letter 'a'
