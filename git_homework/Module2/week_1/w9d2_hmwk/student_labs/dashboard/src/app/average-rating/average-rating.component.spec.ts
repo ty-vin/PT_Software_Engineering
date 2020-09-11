c14fa3ced7ff6ad1fdf298192d61cc944d7e6a39
ty-vin <tyvin888@gmail.com>
2020-09-11 00:50:39 -0400

    Mod2 & Mod 3 files

---
+++ git_homework/Module2/week_1/w9d2_hmwk/student_labs/dashboard/src/app/average-rating/average-rating.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { AverageRatingComponent } from './average-rating.component';
+
+describe('AverageRatingComponent', () => {
+  let component: AverageRatingComponent;
+  let fixture: ComponentFixture<AverageRatingComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ AverageRatingComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AverageRatingComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ git_homework/Module2/week_1/w9d2_hmwk/student_labs/dashboard/src/app/website-visitors/website-visitors.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { WebsiteVisitorsComponent } from './website-visitors.component';
+
+describe('WebsiteVisitorsComponent', () => {
+  let component: WebsiteVisitorsComponent;
+  let fixture: ComponentFixture<WebsiteVisitorsComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ WebsiteVisitorsComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(WebsiteVisitorsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
